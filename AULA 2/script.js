90293f7df95c8021f262866a026d57a6c9c45887
ItalloMene <italloborgessilva22@gmail.com>
2025-02-12 16:52:39 -0300

    adição da opração lógica if else ao JavaScript

--- AULA 2/script.js
+++ AULA 2/script.js
@@ -4,17 +4,27 @@ function adicionarTarefa() {
     let inputTarefa = document.getElementById("inputTarefa")
     let tarefa = inputTarefa.value
 
-    //Cria novo item (li) e insere na (lista ul)
-    document.getElementById("mensagem").textContent = tarefa;
-    let listaTarefas = document.getElementById("listaTarefas")
-    let novaTarefa = document.createElement("li")
-    novaTarefa.textContent = tarefa
-    listaTarefas.appendChild(novaTarefa)
+    // Se o valor do input for vazio então mostre uma mensagem de erro para o usuário
+    if (tarefa == "") {
+        //mostre uma mensagem de erro
+        let mensagemErro = "Digite uma tarefa para adiciona-la a sua lista!"
+        document.getElementById("mensagem").textContent = mensagemErro
 
-    //mensagem de tarefa adicionada com sucesso
-    let mensagem = "Tarefa adicionada com sucesso!"
-    document.getElementById("mensagem").textContent = mensagem
+    } else {
+        // Se o input for preenchido, mostre uma mensagem de sucesso
+        let mensagemSucesso = "Tarefa adicionada com sucesso!"
+        document.getElementById("mensagem").textContent = mensagemSucesso
 
+         //Cria novo item (li) e insere na (lista ul)
+        let listaTarefas = document.getElementById("listaTarefas")
+        let novaTarefa = document.createElement("li")
+        novaTarefa.textContent = tarefa
+        listaTarefas.appendChild(novaTarefa)
+    }
+    
     //Limpa o input
     inputTarefa.value = ""
+
+
 }
+
