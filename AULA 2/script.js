12d27ad5746b107a2b6c21cd779d94fde7d6828d
ItalloMene <italloborgessilva22@gmail.com>
2025-02-12 11:46:13 -0300

    adição de comentarios sobre as linhas de códigos no JavaScript

--- AULA 2/script.js
+++ AULA 2/script.js
@@ -1,9 +1,25 @@
 function adicionarTarefa() {
-    let mensagem = "Tarefa adicionada com sucesso!";
-    document.getElementById("mensagem").textContent = mensagem;
+    //let mensagem = "Tarefa adicionada com sucesso!";
 
-    let inputTarefa = document.getElementById("inputTarefa")
+    //pega o valor do input da tarefa
+    let inputTarefa = document.getElementById("inputTarefa")    
     let tarefa = inputTarefa.value
-    console.log(tarefa)
+    
+    //adiciona a tarefa (valor do inputTarefa) ao paragrafo mensagem
+    document.getElementById("mensagem").textContent = tarefa;
+    
+    //Acesso á lista (ul) no HTML
+    let listaTarefas = document.getElementById("listaTarefas")
+    
+    //Cria um tópico á lista (li) no HTML
+    let novaTarefa = document.createElement("li")
 
+    //Adiciona a tarefa (valor do inputTarefa) ao tópico (li)
+    novaTarefa.textContent = tarefa
+
+    //Adiciona o tópico "li" (Elemento filho/ novaTarefa) na lista "ul" (Elemento pai/ listaTarefas)
+    listaTarefas.appendChild(novaTarefa)
+
+    //Limpa o input
+    inputTarefa.value = ""
 }
