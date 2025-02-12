30a427f6db98149c050693512c535cedcbbe5e86
ItalloMene <italloborgessilva22@gmail.com>
2025-02-12 17:11:31 -0300

    ajuste na variavel inputTarefa e listaTarefas e mensagem

--- AULA 2/script.js
+++ AULA 2/script.js
@@ -1,27 +1,29 @@
 function adicionarTarefa() {
 
     //Recebe o valor do input do usuário
-    let inputTarefa = document.getElementById("inputTarefa")
-    let tarefa = inputTarefa.value
+    const inputTarefa = document.getElementById("inputTarefa")
+    let tarefa = inputTarefa.value.trim()
+    
+    let mensagem = document.getElementById("mensagem")
 
     // Se o valor do input for vazio então mostre uma mensagem de erro para o usuário
     if (tarefa == "") {
         //mostre uma mensagem de erro
         let mensagemErro = "Digite uma tarefa para adiciona-la a sua lista!"
-        document.getElementById("mensagem").textContent = mensagemErro
+        mensagem.textContent = mensagemErro
 
     } else {
         // Se o input for preenchido, mostre uma mensagem de sucesso
         let mensagemSucesso = "Tarefa adicionada com sucesso!"
-        document.getElementById("mensagem").textContent = mensagemSucesso
+        mensagem.textContent = mensagemSucesso
 
          //Cria novo item (li) e insere na (lista ul)
-        let listaTarefas = document.getElementById("listaTarefas")
+        const listaTarefas = document.getElementById("listaTarefas")
         let novaTarefa = document.createElement("li")
         novaTarefa.textContent = tarefa
         listaTarefas.appendChild(novaTarefa)
     }
-    
+
     //Limpa o input
     inputTarefa.value = ""
 
