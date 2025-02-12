f3aff6300a52156b4d535a272b8aa46d5e906a09
ItalloMene <italloborgessilva22@gmail.com>
2025-02-12 16:13:51 -0300

    ajustes comentarios

--- AULA 2/script.js
+++ AULA 2/script.js
@@ -1,16 +1,18 @@
 function adicionarTarefa() {
     //let mensagem = "Tarefa adicionada com sucesso!";
 
+    //Acessa o input da tarefa
+    let inputTarefa = document.getElementById("inputTarefa")
+
     //pega o valor do input da tarefa
-    let inputTarefa = document.getElementById("inputTarefa")    
     let tarefa = inputTarefa.value
-    
+
     //adiciona a tarefa (valor do inputTarefa) ao paragrafo mensagem
     document.getElementById("mensagem").textContent = tarefa;
-    
+
     //Acesso á lista (ul) no HTML
     let listaTarefas = document.getElementById("listaTarefas")
-    
+
     //Cria um tópico á lista (li) no HTML
     let novaTarefa = document.createElement("li")
 
